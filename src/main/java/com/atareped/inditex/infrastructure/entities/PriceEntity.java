e81f6a59432d391cbce5d34650cb3d19d668c098
Pedro Atarés <pedroataresrodriguez@hotmail.com>
2025-03-15 13:35:46 +0100

    fixed data injection issue

--- src/main/java/com/atareped/inditex/infrastructure/entities/PriceEntity.java
+++ src/main/java/com/atareped/inditex/infrastructure/entities/PriceEntity.java
@@ -2,6 +2,8 @@ package com.atareped.inditex.infrastructure.entities;
 
 import com.atareped.inditex.domain.models.Price;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,13 +12,14 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Entity
+@Entity(name = "prices")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class PriceEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int brandId;
     private LocalDateTime startDate;
